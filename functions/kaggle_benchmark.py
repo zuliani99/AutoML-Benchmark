86d8836034d56f3e10ec68d5f63f4fca63e665b0
Riccardo <riccardo.zuliani99@gmail.com>
2021-04-21 14:38:27 +0200

    FIx kaggle benchmark start

--- functions/kaggle_benchmark.py
+++ functions/kaggle_benchmark.py
@@ -7,11 +7,18 @@ from datetime import datetime
 from utils.algo_functions import fun_autosklearn, fun_tpot, fun_h2o, fun_autokeras, fun_autogluon
 from utils.usefull_functions import scatter
 
-def kaggle_benchmark():
+datasets = [('titanic', 'classification'), ('tabular-playground-series-mar-2021', 'classification')]
+
+def get_task(df):
+    for d in datasets:
+        if df == d[0]:
+            return d[1]
+    return False
+
+def kaggle_benchmark(list_df):
     api = KaggleApi()
     api.authenticate()
 
-    datasets = [('titanic', 'classification'), ('tabular-playground-series-mar-2021', 'classification')]
 
     res_class = {'dataset': [], 'autosklearn': [], 'tpot': [], 'autokeras': [], 'h2o': [], 'autogluon': [], 'best': []}
     res_reg = {'dataset': [], 'autosklearn': [], 'tpot': [], 'autokeras': [], 'h2o': [], 'autogluon': [], 'best': []}
@@ -20,43 +27,49 @@ def kaggle_benchmark():
     res_reg = pd.DataFrame(res_reg)
 
 
-    for dataset in datasets:
-        print('------------------Dataset name: ' + dataset[0] + ' - Task: ' + dataset[1] + '------------------')
-        path = './datasets/kaggle/' + dataset[0]
+    for df in list_df:
+        task = get_task(df)
+        if task is not False:
+            print('------------------Dataset name: ' + df + ' - Task: ' + task + '------------------')
+            path = './datasets/kaggle/' + df
 
-        api.competition_download_files(dataset[0], path=path)
+            api.competition_download_files(df, path=path)
 
-        zf = ZipFile(path + '/' + dataset[0] + '.zip')
-        zf.extractall(path) #save files in selected folder
-        zf.close()
+            zf = ZipFile(path + '/' + df + '.zip')
+            zf.extractall(path) #save files in selected folder
+            zf.close()
 
-        os.remove(path + '/' + dataset[0] + '.zip')
+            os.remove(path + '/' + df + '.zip')
 
-        train = pd.read_csv(path + '/train.csv')
-        test = pd.read_csv(path + '/test.csv')
+            train = pd.read_csv(path + '/train.csv')
+            test = pd.read_csv(path + '/test.csv')
 
 
-        res = [fun_autosklearn((train, test), dataset[1]), fun_tpot((train, test), dataset[1]), fun_autokeras((train, test), dataset[1]), fun_h2o((train, test), dataset[1]), fun_autogluon((train, test), dataset[1])]
+            res = [fun_autosklearn((train, test), task), fun_tpot((train, test), task), fun_autokeras((train, test), task), fun_h2o((train, test), task), fun_autogluon((train, test), task)]
 
-        if(dataset[1] == 'classification'):
-            new_row = {'dataset': dataset[0], 'autosklearn': res[0],'tpot': res[1], 'autokeras': res[2], 'h2o': res[3], 'autogluon': res[4], 'best': res_class.columns[np.argmax(res)+1] }
-            res_class = res_class.append(new_row, ignore_index=True)
+            if(task == 'classification'):
+                new_row = {'dataset': df, 'autosklearn': res[0],'tpot': res[1], 'autokeras': res[2], 'h2o': res[3], 'autogluon': res[4], 'best': res_class.columns[np.argmax(res)+1] }
+                res_class = res_class.append(new_row, ignore_index=True)
+            else:
+                new_row = {'dataset': df, 'autosklearn': res[0],'tpot': res[1], 'autokeras': res[2], 'h2o': res[3], 'autogluon': res[4], 'best': res_class.columns[np.argmin(res)+1] }
+                res_reg = res_reg.append(new_row, ignore_index=True)
         else:
-            new_row = {'dataset': dataset[0], 'autosklearn': res[0],'tpot': res[1], 'autokeras': res[2], 'h2o': res[3], 'autogluon': res[4], 'best': res_class.columns[np.argmin(res)+1] }
-            res_reg = res_reg.append(new_row, ignore_index=True)
+            print('\nDatasek di kaggle inesistente. Se esistente accertarsi di aver accettato le condizioni della competizione.\n')
 
-
-    print('---------------------------------RISULTATI DI CLASSIFICAZIONE KAGGLE---------------------------------')
-    print(res_class)
-    print('\n\n---------------------------------RISULTATI DI REGRESSIONE KAGGLE---------------------------------')
-    print(res_reg)
 
     path = './results/kaggle/' + str(datetime.now())
     os.makedirs(path)
     if(not res_class.empty):
+        print('---------------------------------RISULTATI DI CLASSIFICAZIONE KAGGLE---------------------------------')
+        print(res_class)
+
         res_class.to_csv(path + '/classification.csv', index = False)
         scatter(res_class, 'Kaggle - Classificazione')
+
     if(not res_reg.empty):
+        print('\n\n---------------------------------RISULTATI DI REGRESSIONE KAGGLE---------------------------------')
+        print(res_reg)
+
         res_reg.to_csv(path + '/regression.csv', index = False)
         scatter(res_reg, 'Kaggle - Regressione')
 
--- start.py
+++ start.py
@@ -5,18 +5,17 @@ from functions.kaggle_benchmark import kaggle_benchmark
 from functions.test import test
 import sys
 import argparse
+import os
 
 
 def main():
     print("---------------------------------------START---------------------------------------")
     parser = argparse.ArgumentParser()
     parser.add_argument("--ndfopenml", help="number of opneml's dataframes to test")
-    parser.add_argument("--dfkaggle", help="list of kaggle's dataframes to test")
+    parser.add_argument("--dfkaggle", nargs="+", help="list of kaggle's dataframes to test: " + str(os.listdir('./datasets/kaggle')))
     parser.add_argument("--id", help="id of dataset to test")
     parser.add_argument("--algo", help="algorithm to use: autosklearn, tpot, h2o, autokeras, autogluon and all")
     args = parser.parse_args()
-    
-    #print(len(sys.argv))
 
     if args.id and args.algo and len(sys.argv)-1 == 4:
         #print('test')
@@ -24,8 +23,9 @@ def main():
     elif args.ndfopenml and len(sys.argv)-1 == 2:
         #print('openml')
         openml_benchmark(int(args.ndfopenml))
-    elif args.dfkaggle and len(sys.argv)-1 == 2:
-        print('kaggle')
+    elif args.dfkaggle and len(sys.argv) > 2:
+        #print('kaggle ' + str(args.dfkaggle))
+        kaggle_benchmark(args.dfkaggle)
     else:
         print('Comando non valido!')
 
