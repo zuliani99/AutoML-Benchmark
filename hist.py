7f4272dd120738acd8270324863bc5fb8f0c037e
Riccardo <riccardo.zuliani99@gmail.com>
2021-04-23 23:15:03 +0200

    ignore all .csv file

--- hist.py
+++ hist.py
@@ -2,5 +2,5 @@ import pandas as pd
 from utils.usefull_functions import hist
 
 
-data = pd.read_csv('./results/kaggle/2021-04-22 10:42:04.270430/classification.csv')
-hist(data, 'Kaggel - Classificazione')
+data = pd.read_csv('./results/openml/2021-04-23 18:21:21.381440/regression.csv')
+hist(data, 'Openml - Reressione')
