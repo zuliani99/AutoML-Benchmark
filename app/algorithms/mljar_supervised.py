190326316e269fb9dd08e105bbfba9d3ee6b0c9c
Riccardo <riccardo.zuliani99@gmail.com>
2021-08-10 15:23:29 +0200

    fix mljar again

--- app/algorithms/mljar_supervised.py
+++ app/algorithms/mljar_supervised.py
@@ -39,8 +39,8 @@ def make_pipeline_mljar(pipelines, dirs):
   ens = ''
   for dict in ['Ensemble', 'Ensamble_Staked']:
     if dirs.get(dict) is not None:
-      md = re.split('## Ensemble structure|## Metric details|## Confusion matrix',dirs.get(dict))
-      ens += '\n\n#### ' + dict + ' Statistics\n' + md[1] + md[2]
+      md = re.split('## Ensemble structure|## Confusion matrix|## Learning curves',dirs.get(dict))
+      ens += '\n\n## ' + dict + ' Structure\n' + md[1]
 
   return str(pipelines.to_markdown() + ens)
 
@@ -69,18 +69,27 @@ def mljar(df, task, options, time_start):
 def do_mljar(df, options, task, time_start):
   df_new = copy.copy(df) # Deep copy of the DataFrame passed to parameter
   df_new = fill_and_to_category(df_new) # Initial cleaning of the DataFrame
+  #df_new = get_list_single_df(df_new) # Initial cleaning of the DataFrame
+
   pd.options.mode.chained_assignment = None
 
   X, y = return_X_y(df_new) # Obtain the two DataFrame X and y needed to execute the train_test_split
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-  automl = AutoML(mode="Compete", total_time_limit=60*options['time'])
+
+  # Specifying the ml task
+  if task == 'classification':
+    if len(np.unique(y)) > 2: ml_task = 'multiclass_classification'
+    else: ml_task = 'binary_classification'
+  else: ml_task=task
+
+  automl = AutoML(mode="Compete", total_time_limit=60*options['time'], ml_task=ml_task)
   
   automl.fit(X_train, y_train)
   y_pred = automl.predict_all(X_test)
  
   dirs = get_dirs()
   pipelines = make_pipeline_mljar(pd.read_csv('AutoML_1/leaderboard.csv'), dirs) # Pipelines
-  #shutil.rmtree('./AutoML_1') # Deleting the folder created for saving the models tested by MLJAR
+  shutil.rmtree('./AutoML_1') # Deleting the folder created for saving the models tested by MLJAR
 
   print('---------------------------------MLJAR---------------------------------\n\n')
 
@@ -88,6 +97,7 @@ def do_mljar(df, options, task, time_start):
 
   if task != 'classification':
     return (round(np.sqrt(mean_squared_error(y_test, y_pred)), 3), round(r2_score(y_test, y_pred), 3), pipelines, time_elapsed)
+
   # Check if it is a binary or multilables case
   if len(np.unique(y)) > 2:
     return (round(accuracy_score(y_test, y_pred['label']), 3), round(f1_score(y_test, y_pred['label'], average='weighted'), 3), pipelines, time_elapsed)
